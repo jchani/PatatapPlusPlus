9bd58b8ca8ebb7a1aea38d9234e703ab02c44b98
Justin Chan <justinchan@WL-215-188.WPA.Claremont.Edu>
2016-12-12 01:32:08 -0800

    Adding sounds and almost finished recording aspect

--- Animations/sketch.js
+++ Animations/sketch.js
@@ -1,13 +1,37 @@
-var row = 0; var col = 0; //row and column of the current key press (col set to 10 so no animations)
+var row = 0; //row of the key 
 var colour;
 var canvasColor = [127, "#00BFFF","#F5F5DC"]; // Array of colors to cycle through: Blue, Beige,
 var canvasIndex = 0; 
 var sound; //current sound
 
+var current; //current recordObj
+var currentTime;
+var recordMode = false; //used to toggle between real-time and record mode
+var recording = []; //stores recordObj objects
+var addIndex = 0; 
+var loopIndex; var loopTime; //used when looping
+var timeBarDX = 0;
+
 function preload(){
-  sound1 = loadSound('sounds/pop.mp3');
-  sound2 = loadSound('sounds/pop.mp3');
-  sound3 = loadSound('sounds/pop.mp3');  
+  sound1 = loadSound('assets/Echo.mp3');
+  sound2 = loadSound('assets/glock1.wav');
+  sound3 = loadSound('assets/Vibraslap.mp3');  
+  sound4 = loadSound('assets/Toot.mp3'); 
+  sound5 = loadSound('assets/glock.wav');   
+  
+  sound11 = loadSound('assets/ConcreteThump.wav'); 
+  sound12 = loadSound('assets/bass2.wav'); 
+  sound13 = loadSound('assets/OpenHiHat.wav'); 
+  sound14 = loadSound('assets/ViolinFade.mp3'); 
+  sound15 = loadSound('assets/Snare.wav');  
+  
+  sound20 = loadSound('assets/FloorTom.wav');  
+  sound21 = loadSound('assets/bass1.wav');  
+  sound22 = loadSound('assets/OpenHiHat.wav');  
+  sound23 = loadSound('assets/FingerSnap.wav');  
+  sound24 = loadSound('assets/EDBass.wav');   
+  
+  noSound = loadSound('assets/silence.mp3')
 }
 
 function setup() {
@@ -35,130 +59,190 @@ function draw() {
   animateColorWheel();
   animateShootingLine();
   animateFadingRectangle();
+  
+  if(recordMode){
+    animateTimeBar();
+    loopRecording();
+  }
 }
-
 
 function keyTyped() { 
   println("Key typed is: " + int(keyCode))
-  switch(keyCode){
+  
+  //Return key used to toggle recording mode
+  if(keyCode === 13){
+    recordMode = !recordMode; //toggle recordMode
+    timeBarDX = 0;
+    loopIndex = 0; 
+    
+  }
+  
+  //store recordObj objects
+  if(recordMode && keyCode !== (0 || 13)){//will be 0 if no key pressed so exclude this case
+    recording[addIndex] = new recordObj(millis(), keyCode);
+    println("Size of recording array is " + recording.length);
+    println("Array elements are: " + printRecordingArray());
+    //println("Time in ms: " + millis());
+    addIndex++;
+  }
+  
+  keyMapping(keyCode);
+  sound.play();
+}
+
+//Recording Aspects below
+//stores data on recorded animation
+function recordObj(time, code){
+  this.time = time;
+  this.code = code;
+}
+
+function animateTimeBar(){
+  fill('black');
+  rect(0, height - (height/50), timeBarDX, height/50);   
+  if(timeBarDX < width){
+    timeBarDX += width/600;
+  }
+}
+
+function restartLoop(){
+  timeBarDX = 0;
+}
+
+function loopRecording(){
+  if(recording.length > 0){
+    if(recording.length === 1){
+      current = recording[loopIndex];
+      loopIndex++;
+    }
+    
+    currentTime = millis();  
+    
+    if(current.time %10000 < currentTime%10000){//duration of loop is 10 seconds (10000 ms)
+      keyMapping(current.code); //play animation+sound
+      current = recording[loopIndex]; //
+      currentTime = millis();
+      //println("current.code is " + current.code);
+      //println("currentTime is: " + currentTime);  
+      loopIndex++;
+    }
+    
+
+   
+    // if((current.time > previousTime%10000)&&(current.time <= currentTime%10000)){
+    //   keyMapping(current.code); //play animation+sound
+    //   previousTime = currentTime; 
+    // }
+  }
+}
+
+function printRecordingArray(){
+  var str = "";
+  for(var i = 0; i < recording.length; i++){
+    str = str + "Time is " + recording[i].time + ", Code is " + recording[i].code;
+  }
+  return str;
+}
+
+
+
+//handles key presses for real-time animations
+function keyMapping(keyNum){
+    switch(keyNum){
     case 113: //Q
       row = 0;     
-      col = 0;
       sound = sound1;
       resetFallingCircles();
       break;   
     case 119: //W
       row = 0;
-      col = 1;
       sound = sound2;
       showStar = 50;
       break;
     case 101: //E
       row = 0;
-      col = 2;
-      sound = sound2;
+      sound = sound3;
       showColWheel = 50;
       resetColorWheel();
       break;
     case 114: //R
       row = 0;
-      col = 3;
-      sound = sound1;
+      sound = sound4;
       showLine = 40;
       resetShootingLine();
       break;
     case 116: ///T
       row = 0;
-      col = 4;
-      sound = sound1;
+      sound = sound5;
       resetFadingRectangle();
       break;
       
+      
     case 97: //A
       row = 1;
-      col = 0;
-      //sound = sound11;      
+      sound = sound11;      
       resetFallingCircles();
       break;
     case 115: //S
       row = 1;
-      col = 1;
-      sound = sound2;
+      sound = sound12;
       showStar = 50;
       break;    
     case 100: //D
       row = 1;
-      col = 2;
-      sound = sound2;
+      sound = sound13;
       showColWheel = 50;
       resetColorWheel();      
       break;
     case 102: //F
       row = 1;
-      col = 3;
-      sound = sound2;
+      sound = sound14;
       showLine = 40;
       resetShootingLine();      
       break;
     case 103: //G
       row = 1;
-      col = 4;
-      sound = sound2;
+      sound = sound15;
       showRect = 30;
       resetFadingRectangle();      
       break;
                   
-      
-
+    
     case 122: //Z
       row = 2;
-      col = 0;
-      //sound = sound20;
+      sound = sound20;
       resetFallingCircles();
       break;
     case 120: //X
       row = 2;
-      col = 1;
-      sound = sound2;
+      sound = sound21;
       showStar = 50;
       break;      
     case 99: //C
       row = 2;
-      col = 2;
-      showColWheel = 50;
+      sound = sound22;
       resetColorWheel(); 
       break;       
     case 118: //V
       row = 2;
-      col = 3;
+      sound = sound23;
       showLine = 40;
       resetShootingLine(); 
       break;          
-    case 98: //V
+    case 98: //B
       row = 2;
-      col = 4;
-      showRect = 30;
+      sound = sound24;
       resetFadingRectangle(); 
-      break;             
+      break;           
+      
     //Spacebar
     case 32:
       row = 4;
-      col = 11; //spacebar
-      sound = sound1;
+      sound = noSound;
       changeCanvasColor();
       break;
   }
-  sound.play();
-  println(canvasColor[canvasIndex%canvasColor.length])
 }
-
-
-
-
-
-
-
-
 
 
 
--- musicalpainting/sketch.js
+++ musicalpainting/sketch.js
@@ -1,27 +1,41 @@
-
-var row = 0; var col = 0; //row and column of the current key press (col set to 10 so no animations)
+var row = 0; //row of the key 
 var colour;
-var canvasColor = ["#00BFFF","#F5F5DC"]; // Array of colors to cycle through: Blue, Beige,
+var canvasColor = [127, "#00BFFF","#F5F5DC"]; // Array of colors to cycle through: Blue, Beige,
 var canvasIndex = 0; 
 var sound; //current sound
 
-var showStar = 0;var showColWheel = 0; //ints that will be decremented: 0 - no animation, any other number - animation
-
-var fallingCircles = []; // Objects that move
-var numfallingCircles = 0; //Number of objects in fallingCircles array
-
-
-
-var liquid; // Liquid
+var current; //current recordObj
+var currentTime;
+var recordMode = false; //used to toggle between real-time and record mode
+var recording = []; //stores recordObj objects
+var addIndex = 0; 
+var loopIndex; var loopTime; //used when looping
+var timeBarDX = 0;
 
 function preload(){
-  sound1 = loadSound('sounds/pop.mp3');
-  sound2 = loadSound('sounds/pop.mp3');
-  sound3 = loadSound('sounds/pop.mp3');  
+  sound1 = loadSound('assets/Echo.mp3');
+  sound2 = loadSound('assets/glock1.wav');
+  sound3 = loadSound('assets/Vibraslap.mp3');  
+  sound4 = loadSound('assets/Toot.mp3'); 
+  sound5 = loadSound('assets/glock.wav');   
+  
+  sound11 = loadSound('assets/ConcreteThump.wav'); 
+  sound12 = loadSound('assets/bass2.wav'); 
+  sound13 = loadSound('assets/OpenHiHat.wav'); 
+  sound14 = loadSound('assets/ViolinFade.mp3'); 
+  sound15 = loadSound('assets/Snare.wav');  
+  
+  sound20 = loadSound('assets/FloorTom.wav');  
+  sound21 = loadSound('assets/bass1.wav');  
+  sound22 = loadSound('assets/OpenHiHat.wav');  
+  sound23 = loadSound('assets/FingerSnap.wav');  
+  sound24 = loadSound('assets/EDBass.wav');   
+  
+  noSound = loadSound('assets/silence.mp3')
 }
 
 function setup() {
-  createCanvas(640, 360);
+  createCanvas(1000, 800);
   background(canvasColor[canvasIndex%canvasColor.length]);
   // Falling animation: Create liquid object
   liquid = new Liquid(0, height/2, width, height/2, 0.1);
@@ -39,235 +53,195 @@ function draw() {
       colour = '#009999';
       break;
   }
-  FallingCircles(colour);
-  shiningStar();
-  colorWheel();
+  animateFallingCircles(colour);
+  animateShiningStar();
+  animateColorWheel();
+  animateShootingLine();
+  animateFadingRectangle();
   
+  if(recordMode){
+    animateTimeBar();
+    loopRecording();
+  }
 }
-
 
 function keyTyped() { 
   println("Key typed is: " + int(keyCode))
-  switch(keyCode){
-    //Q-P
-    case 113:
+  
+  //Return key used to toggle recording mode
+  if(keyCode === 13){
+    recordMode = !recordMode; //toggle recordMode
+    timeBarDX = 0;
+    loopIndex = 0; 
+    
+  }
+  
+  //store recordObj objects
+  if(recordMode && keyCode !== (0 || 13)){//will be 0 if no key pressed so exclude this case
+    recording[addIndex] = new recordObj(millis(), keyCode);
+    println("Size of recording array is " + recording.length);
+    println("Array elements are: " + printRecordingArray());
+    //println("Time in ms: " + millis());
+    addIndex++;
+  }
+  
+  keyMapping(keyCode);
+  sound.play();
+}
+
+//Recording Aspects below
+//stores data on recorded animation
+function recordObj(time, code){
+  this.time = time;
+  this.code = code;
+}
+
+function animateTimeBar(){
+  fill('black');
+  rect(0, height - (height/50), timeBarDX, height/50);   
+  if(timeBarDX < width){
+    timeBarDX += width/600;
+  }
+}
+
+function restartLoop(){
+  timeBarDX = 0;
+}
+
+function loopRecording(){
+  if(recording.length > 0){
+    if(recording.length === 1){
+      current = recording[loopIndex];
+      loopIndex++;
+    }
+    
+    currentTime = millis();  
+    
+    if(current.time %10000 < currentTime%10000){//duration of loop is 10 seconds (10000 ms)
+      keyMapping(current.code); //play animation+sound
+      current = recording[loopIndex]; //
+      currentTime = millis();
+      //println("current.code is " + current.code);
+      //println("currentTime is: " + currentTime);  
+      loopIndex++;
+    }
+    
+
+   
+    // if((current.time > previousTime%10000)&&(current.time <= currentTime%10000)){
+    //   keyMapping(current.code); //play animation+sound
+    //   previousTime = currentTime; 
+    // }
+  }
+}
+
+function printRecordingArray(){
+  var str = "";
+  for(var i = 0; i < recording.length; i++){
+    str = str + "Time is " + recording[i].time + ", Code is " + recording[i].code;
+  }
+  return str;
+}
+
+
+
+//handles key presses for real-time animations
+function keyMapping(keyNum){
+    switch(keyNum){
+    case 113: //Q
       row = 0;     
-      col = 0;
       sound = sound1;
       resetFallingCircles();
       break;   
-    case 119:
+    case 119: //W
       row = 0;
-      col = 1;
       sound = sound2;
       showStar = 50;
       break;
-    case 101: 
+    case 101: //E
       row = 0;
-      col = 2
-      sound = sound2;
+      sound = sound3;
       showColWheel = 50;
+      resetColorWheel();
+      break;
+    case 114: //R
+      row = 0;
+      sound = sound4;
+      showLine = 40;
+      resetShootingLine();
+      break;
+    case 116: ///T
+      row = 0;
+      sound = sound5;
+      resetFadingRectangle();
       break;
       
       
-    //A-S
-    case 97:
+    case 97: //A
       row = 1;
-      col = 0;
-      //sound = sound11;      
+      sound = sound11;      
       resetFallingCircles();
       break;
-    case 115:
+    case 115: //S
       row = 1;
-      col = 1;
-      sound = sound2;
+      sound = sound12;
       showStar = 50;
       break;    
-    //Z-X
-    case 122:
+    case 100: //D
+      row = 1;
+      sound = sound13;
+      showColWheel = 50;
+      resetColorWheel();      
+      break;
+    case 102: //F
+      row = 1;
+      sound = sound14;
+      showLine = 40;
+      resetShootingLine();      
+      break;
+    case 103: //G
+      row = 1;
+      sound = sound15;
+      showRect = 30;
+      resetFadingRectangle();      
+      break;
+                  
+    
+    case 122: //Z
       row = 2;
-      col = 0;
-      //sound = sound20;
+      sound = sound20;
       resetFallingCircles();
       break;
-    case 120:
+    case 120: //X
       row = 2;
-      col = 1;
-      sound = sound2;
+      sound = sound21;
       showStar = 50;
       break;      
+    case 99: //C
+      row = 2;
+      sound = sound22;
+      resetColorWheel(); 
+      break;       
+    case 118: //V
+      row = 2;
+      sound = sound23;
+      showLine = 40;
+      resetShootingLine(); 
+      break;          
+    case 98: //B
+      row = 2;
+      sound = sound24;
+      resetFadingRectangle(); 
+      break;           
       
     //Spacebar
     case 32:
       row = 4;
-      col = 11; //spacebar
-      sound = sound1;
+      sound = noSound;
       changeCanvasColor();
       break;
   }
-  sound.play();
-  println(canvasColor[canvasIndex%canvasColor.length])
 }
-
-function changeCanvasColor(){
-  canvasIndex++;
-  background(canvasColor[canvasIndex%canvasColor.length]);
-}
-
-function shiningStar(){
-  if(showStar > 0){
-    var x = random(0,width);
-    var y = random(0,height);
-    stroke(0);
-    strokeWeight(5);
-    line(x,y,width/2,height/2);
-    strokeWeight(1);
-    fill(colour);    
-    ellipse(width/2,height/2,20,20);
-    //strokeWeight(0);
-    //ellipse(x,y,40,40);
-    showStar--;
-  }
-}
-
-
-function movingCircle(){
-  
-}
-
-function flashingCircles(){
-  
-}
-
-function colorWheel(){
-	if(showColWheel > 0){
-  	if(frameCount % 1 === 0) {
-  		fill(random(255), random(255), random(255));
-  		push();
-  		translate(200, 200);
-  		rotate(radians(frameCount*20));
-  		rect(0, 0, 100, 20);
-  		pop();
-  		showColWheel--;
-  	}
-	}
-	
-
-	
-}
-
-// Restart all the Mover objects randomly
-function resetFallingCircles() {
-  for (var i = 0; i < 3; i++) {
-    fallingCircles[i] = new Mover(random(1, 3), random(600), 0);
-  }
-}
-
-
-//Falling circles animation
-function FallingCircles(colour){
-  
-  // Draw water
-  liquid.display();
-
-  for (var i = 0; i < fallingCircles.length; i++) {
-    
-    // Is the Mover in the liquid?
-    if (liquid.contains(fallingCircles[i])) {
-      // Calculate drag force
-      var dragForce = liquid.calculateDrag(fallingCircles[i]);
-      // Apply drag force to Mover
-      fallingCircles[i].applyForce(dragForce);
-    }
-
-    // Gravity is scaled by mass here!
-    var gravity = createVector(0, 0.5*fallingCircles[i].mass);
-    // Apply gravity
-    fallingCircles[i].applyForce(gravity);
-   
-    // Update and display
-    fallingCircles[i].update();
-    fallingCircles[i].display(colour);
-  }  
-}
-
-
-function Liquid(x, y, w, h, c) {
-  this.x = x;
-  this.y = y;
-  this.w = w;
-  this.h = h;
-  this.c = c;
-}
-  
-// Is the Mover in the Liquid?
-Liquid.prototype.contains = function(m) {
-  var l = m.position;
-  return l.x > this.x && l.x < this.x + this.w &&
-         l.y > this.y && l.y < this.y + this.h;
-};
-  
-// Calculate drag force
-Liquid.prototype.calculateDrag = function(m) {
-  // Magnitude is coefficient * speed squared
-  var speed = m.velocity.mag();
-  var dragMagnitude = this.c * speed * speed;
-
-  // Direction is inverse of velocity
-  var dragForce = m.velocity.copy();
-  dragForce.mult(-1);
-  
-  // Scale according to magnitude
-  // dragForce.setMag(dragMagnitude);
-  dragForce.normalize();
-  dragForce.mult(dragMagnitude);
-  return dragForce;
-};
-  
-Liquid.prototype.display = function() {
-  noStroke();
-  noFill();
-  rect(this.x, this.y, this.w, this.h);
-};
-
-function Mover(m,x,y) {
-  this.mass = m;
-  this.position = createVector(x,y);
-  this.velocity = createVector(0,0);
-  this.acceleration = createVector(0,0);
-}
-
-// Newton's 2nd law: F = M * A
-// or A = F / M
-Mover.prototype.applyForce = function(force) {
-  var a = p5.Vector.div(force,this.mass);
-  this.acceleration.add(a);
-};
-  
-Mover.prototype.update = function() {
-  // Velocity changes according to acceleration
-  this.velocity.add(this.acceleration);
-  // position changes by velocity
-  this.position.add(this.velocity);
-  // We must clear acceleration each frame
-  this.acceleration.mult(0);
-};
-
-Mover.prototype.display = function(colour) {
-  stroke(0);
-  strokeWeight(2);
-  fill(colour);
-  ellipse(this.position.x,this.position.y,this.mass*16,this.mass*16);
-};
-
-
-
-
-
-
-
-
-
 
 
 
